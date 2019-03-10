96b848a84ccb77a57d855a5d73cb75b358958740
seeminsyed <seemin711@gmail.com>
2019-03-09 20:36:06 -0500

    Layout files for Fridge and Shopping List. RecycleView for Fridge. Class for Fridge Items and a class for Date.

--- WhatsInMyFridge/app/src/main/java/com/example/android/whatsinmyfridge/FridgeFragment.java
+++ WhatsInMyFridge/app/src/main/java/com/example/android/whatsinmyfridge/FridgeFragment.java
@@ -1,17 +1,45 @@
 package com.example.android.whatsinmyfridge;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class FridgeFragment extends Fragment {
-    @Nullable
+
+    private FridgeItem[] mItems;
+    private RecyclerView mRecyclerView;
+    private FridgeAdapter mAdapter;
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_fridge, container, false);
+    public View onCreateView( LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
+
+        // this is data for recycler view
+        FridgeItem[] itemsData = {
+                new FridgeItem("Indigo", new myDate(1, 2, 2019)),
+                new FridgeItem("Red", new myDate(3, 5, 2011)),
+                new FridgeItem("Blue", new myDate(3, 5, 2017)),
+                new FridgeItem("Green", new myDate(3, 5, 2014)),
+                new FridgeItem("Amber", new myDate(3, 5, 2016)),
+                new FridgeItem("Deep Orange", new myDate(3, 5, 2020))};
+
+        View rootView = inflater.inflate(R.layout.fragment_fridge, container, false);
+
+        // Get a handle to the RecyclerView.
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerview);
+
+        // Create an adapter and supply the data to be displayed.
+        mAdapter = new FridgeAdapter(getActivity(), itemsData);
+
+        // Connect the adapter with the RecyclerView.
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Give the RecyclerView a default layout manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return rootView;
     }
 }
---
+++ WhatsInMyFridge/app/src/main/java/com/example/android/whatsinmyfridge/FridgeItem.java
@@ -0,0 +1,22 @@
+package com.example.android.whatsinmyfridge;
+
+import java.util.Calendar;
+
+public class FridgeItem {
+    public String name;
+    public myDate expiration;
+    public int daysLeft;
+
+
+    /**
+     * Constructor
+     *
+     * @param name String
+     * @param expiration
+     */
+    public FridgeItem(String name, myDate expiration) {
+        this.name = name;
+        this.expiration = expiration;
+        this.daysLeft = expiration.difference(expiration);
+    }
+}
